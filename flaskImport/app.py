53844603a39cea0311881374c72ff0c4c2337176
Darron Anderson <flameshadow98@yahoo.com>
2022-02-22 21:41:27 -0600

    Update app.py

--- flaskImport/app.py
+++ flaskImport/app.py
@@ -85,7 +85,7 @@ def s3uploading(filename, filenameWithPath):
                 Bucket=bucket, Key=path_filename)
 
     return "http://"+BUCKET_NAME+\
-            ".s3-website-us-east-1.amazonaws.com/"+ path_filename 
+            ".s3-website-us-east-2.amazonaws.com/"+ path_filename 
 
 @app.route('/', methods=['GET', 'POST'])
 def home_page():
